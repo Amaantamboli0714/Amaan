848ea987bc3728f012b3fa3534e5063e5562f770
Amaantamboli0714 <144790166+Amaantamboli0714@users.noreply.github.com>
2023-09-12 15:30:01 +0530

    Create addition.cpp
    My first program

---
+++ addition.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+cout<<"Entre first number:-"<<endl;
+cin>>a;
+cout<<"Enter second number:-"<<endl;
+cin>>b;
+cout<<"your result is:-"<<a+b;
+
+
+
+}
